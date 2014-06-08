ca83bbbea5d96129ff1915d41c463e3e11c82971
Adriano Waltrick <akuma.majunia@gmail.com>
2014-06-08 14:27:51 -0300

    <? namespace info_data\base;
    use imclass\banco_dados\IMConexaoBancoDados;
    use imclass\banco_dados\IMConexaoAtributos;
    use imclass\banco_dados\IMConexaoBancoDadosPDO;
    
    /**
     * Retorna a conexao com a base local
     */
    class ConexaoLocalIM {
    
       /**
        * Retorna a conexao com a base local
        *
        * @return IMConexaoBancoDados
        */
       function getConexao()
       {
          $objIMConexaoBancoDados = new IMConexaoBancoDadosPDO();
    
          $objIMConexaoBancoDados->conectar(
             $this->getIMConexaoAtributos()
          );
    
          return $objIMConexaoBancoDados;
       }
    
       function getIMConexaoAtributos()
       {
          $objIMConexaoAtributos = new IMConexaoAtributos();
    
          $objIMConexaoAtributos->setNomeBanco("localhost");
          $objIMConexaoAtributos->setLogin("moodle");
          $objIMConexaoAtributos->setSenha("moodle");
          $objIMConexaoAtributos->setBanco("adriano");
          $objIMConexaoAtributos->setHost("localhost");
          $objIMConexaoAtributos->setPorta("");
    
          return $objIMConexaoAtributos;
        }
    
    }
    ?>

---
+++ info_data/base/ConexaoLocalIM.php
@@ -0,0 +1,44 @@
+<?
+namespace info_data\base;
+
+use imclass\banco_dados\IMConexaoBancoDados;
+use imclass\banco_dados\IMConexaoAtributos;
+use imclass\banco_dados\IMConexaoBancoDadosPDO;
+
+/**
+ * Retorna a conexao com a base local
+ */
+class ConexaoLocalIM {
+
+   /**
+    * Retorna a conexao com a base local
+    *
+    * @return IMConexaoBancoDados
+    */
+   function getConexao()
+   {
+      $objIMConexaoBancoDados = new IMConexaoBancoDadosPDO();
+
+      $objIMConexaoBancoDados->conectar( 
+         $this->getIMConexaoAtributos()
+      );
+
+      return $objIMConexaoBancoDados;
+   }
+
+   function getIMConexaoAtributos()
+   {
+      $objIMConexaoAtributos = new IMConexaoAtributos();
+
+      $objIMConexaoAtributos->setNomeBanco("localhost");
+      $objIMConexaoAtributos->setLogin("moodle");
+      $objIMConexaoAtributos->setSenha("moodle");
+      $objIMConexaoAtributos->setBanco("unimestre_horus_branco");
+      $objIMConexaoAtributos->setHost("localhost");
+      $objIMConexaoAtributos->setPorta("");   
+
+      return $objIMConexaoAtributos;
+    }
+
+}
+?>
