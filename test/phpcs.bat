feaa9c8c04f133232c16c70c39da3a1a3a334b89
Adriano Waltrick <akuma.majunia@gmail.com>
2014-06-17 21:53:37 -0300

    phpcs

---
+++ test/phpcs.bat
@@ -0,0 +1,3 @@
+C:\Apache24\php\php ..\externos\PHPCodeSniffer2\scripts\phpcs .\teste.php --encoding=UTF-8
+
+REM --report=full --report-file=phpcs.txt --report-width=75 --tab-width=4 --standard=PSR2 --encoding=UTF-8 --extensions=.php --ignore=apps;externos;info_data;test;views -l -p ../imclass/apps/inputs
